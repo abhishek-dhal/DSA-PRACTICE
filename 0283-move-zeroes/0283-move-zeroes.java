9c0a1d722178e0db6a35426058ce2df8f3d93a32
abhishek-dhal <cse.22bcsh50@silicon.ac.in>
2025-11-26 12:48:53 +0530

    Time: 2 ms (83.56%), Space: 47.4 MB (33.54%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,15 +2,20 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int n=nums.length;
 
-        int write=0;
+        //read ----> for scanning the array
+        //write ----> position of non-zero element
 
+        int write=0;
+       
+       //First we place the non-zero el in order from 0 ----> (write -1)
         for(int read=0;read<n;read++){
             if(nums[read] != 0){
                 nums[write] = nums[read];
                 write++;
             }
         }
-
+       
+       //we handle or place the zero at last from write ---> n-1;
         for(int i=write;i<n;i++){
             nums[i] = 0;
         }
