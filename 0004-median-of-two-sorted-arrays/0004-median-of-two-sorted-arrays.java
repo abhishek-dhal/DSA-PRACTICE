c8a26439b34309ffd198a45d9e08a7d6db2a389a
abhishek-dhal <cse.22bcsh50@silicon.ac.in>
2025-12-14 14:34:58 +0530

    Time: 13 ms (5.71%), Space: 48.8 MB (32.74%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -0,0 +1,38 @@
+class Solution {
+    PriorityQueue<Integer> left;
+    PriorityQueue<Integer> right;
+    public void addNumbers(int number){
+        //first add to left first
+        left.offer(number);
+
+        //then push max of left to right
+        right.offer(left.poll());
+
+        //if at any moment right size becomes greater than left pop from right and push min to left
+        if(right.size() > left.size()){
+            left.offer(right.poll());
+        }
+
+
+    }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        //left ----> max heap
+        left = new PriorityQueue<>((a,b)->b-a);
+
+        //right ----> min heap
+        right = new PriorityQueue<>();
+
+        for(int num:nums1){
+            addNumbers(num);
+        }
+        for(int num: nums2){
+            addNumbers(num);
+        }
+        //if left element numbers odd it contains median
+        if((left.size() + right.size() ) % 2 != 0){
+            return left.peek();
+        }
+        //otherwise left + right/2.0
+        return (left.peek() + right.peek()) /2.0;
+    }
+}
