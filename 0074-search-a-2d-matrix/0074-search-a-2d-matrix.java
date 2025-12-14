54cdb0b6a59a9dac90924ee94386504a0247bc07
abhishek-dhal <cse.22bcsh50@silicon.ac.in>
2025-12-15 01:20:13 +0530

    Time: 0 ms (100%), Space: 43.8 MB (53.24%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int m,n;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        m = matrix.length;
+        n = matrix[0].length;
+
+        int low = 0;
+        int high = m*n -1;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            int row = mid/n;
+            int col = mid%n;
+
+            if(target == matrix[row][col]){
+                return true;
+            }
+
+            else if(target <  matrix[row][col]){
+                high = mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+
+        return false;
+        
+    }
+}
