8b2a2683cb644ef162587136bad7be45f43cd2eb
abhishek-dhal <cse.22bcsh50@silicon.ac.in>
2025-12-15 22:15:55 +0530

    Time: 0 ms (100%), Space: 42 MB (49.5%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,31 @@
 class Solution {
     public int[] dp;
-    public int solve(int ind){
-        if(ind == 0) return dp[ind] = 1;
-        if(ind == 1) return dp[ind] = 1;
+    // public int solve(int ind){
+    //     if(ind == 0) return dp[ind] = 1;
+    //     if(ind == 1) return dp[ind] = 1;
 
-        if(dp[ind] != -1) return dp[ind];
+    //     if(dp[ind] != -1) return dp[ind];
 
-        int left = solve(ind-1);
-        int right = solve(ind-2);
+    //     int left = solve(ind-1);
+    //     int right = solve(ind-2);
 
-        return dp[ind] = left+right;
-    }
+    //     return dp[ind] = left+right;
+    // }
     public int climbStairs(int n) {
+        // dp = new int[n+1];
+        // Arrays.fill(dp,-1);
+        // int m = n;
+        // return solve(m);
+
+        //tabulation
         dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        int m = n;
-        return solve(m);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
     }
 }
