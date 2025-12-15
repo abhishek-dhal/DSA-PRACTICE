ca5f5d4dc856fc3a697a4215045497dfb9f59c5e
abhishek-dhal <cse.22bcsh50@silicon.ac.in>
2025-12-15 21:48:29 +0530

    Time: 0 ms (100%), Space: 41.9 MB (49.5%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dp;
+    public int solve(int ind){
+        if(ind == 0) return dp[ind] = 1;
+        if(ind == 1) return dp[ind] = 1;
+
+        if(dp[ind] != -1) return dp[ind];
+
+        int left = solve(ind-1);
+        int right = solve(ind-2);
+
+        return dp[ind] = left+right;
+    }
+    public int climbStairs(int n) {
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        int m = n;
+        return solve(m);
+    }
+}
