12bf826fe2836cddf48d43864e437568a2493e57
abhishek-dhal <cse.22bcsh50@silicon.ac.in>
2025-12-19 00:54:43 +0530

    Time: 19 ms (78.81%), Space: 56.5 MB (24.59%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int n = people.length;
+        Arrays.sort(people);
+        int i = 0;
+        int j = n - 1;
+        int boat = 0;
+
+        while(i <= j){
+            if(people[i] + people[j] <= limit){
+                i++;
+             // lightest + heaviest can go together
+            }
+            // heaviest always uses a boat
+            j--;
+            boat++;
+        }
+        return boat;
+    }
+}
