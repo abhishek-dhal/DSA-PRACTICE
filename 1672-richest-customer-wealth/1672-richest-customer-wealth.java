8fac2dcfe50a846ff202300022808477679f31a5
abhishek-dhal <cse.22bcsh50@silicon.ac.in>
2026-02-12 17:04:57 +0530

    Time: 2 ms (2.48%), Space: 44.1 MB (98.29%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int findSum(int[] arr){
+
+        int sum = 0;
+
+        for(int i=0;i<arr.length;i++){
+            sum += arr[i];
+        }
+
+        return sum;
+    }
+    public int maximumWealth(int[][] accounts) {
+        
+
+        int m = accounts.length;
+        int n = accounts[0].length;
+
+        int maxi = 0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
+                int value = findSum(accounts[i]);
+
+                maxi = Math.max(maxi,value);
+
+            }
+        }
+
+        return maxi;
+    }
+}
