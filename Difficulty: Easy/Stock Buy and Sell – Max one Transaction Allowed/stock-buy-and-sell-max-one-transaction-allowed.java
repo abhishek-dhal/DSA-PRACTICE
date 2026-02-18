e03cd8954c16fb1f19d9f4526d56eceb21716d55
abhishek-dhal <cse.22bcsh50@silicon.ac.in>
2026-02-18 16:47:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // Code here
+        
+        int n = prices.length;
+        
+        int minPrice = Integer.MAX_VALUE;
+        
+        int maxProfit = 0;
+        
+        for(int i = 0; i < n; i++){
+            
+            if(prices[i] < minPrice){
+                minPrice = prices[i];
+            }
+            
+            int profit = prices[i] - minPrice;
+            
+            maxProfit = Math.max(maxProfit,profit);
+        }
+        return maxProfit;
+    }
+}
