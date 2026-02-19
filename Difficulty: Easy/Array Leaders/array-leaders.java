0d2aec1058de1124a0647e87940f85ab513cd063
abhishek-dhal <cse.22bcsh50@silicon.ac.in>
2026-02-19 11:16:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.java
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -1,21 +1,27 @@
 class Solution {
     static ArrayList<Integer> leaders(int arr[]) {
         // code here
-        int n=arr.length;
+        
+        
+        int n = arr.length;
+        
+        // boolean isYes = true;
+        
+        int maxi = arr[n-1];
+        
         ArrayList<Integer> ans = new ArrayList<>();
         
-        
-        int max_right=-1;
-        
-        for(int i=n-1;i>=0;i--){
-           if(arr[i] >= max_right){
-               ans.add(arr[i]);
-               max_right = arr[i];
-           }
+        for(int i = n - 1; i >= 0; i--){
+            
+             maxi = Math.max(maxi,arr[i]);
+            
+            if(maxi == arr[i]) ans.add(arr[i]);
+            
+           
         }
         
-         Collections.reverse(ans);
-         
-         return ans;
+      Collections.reverse(ans);
+      
+      return ans;
     }
 }
