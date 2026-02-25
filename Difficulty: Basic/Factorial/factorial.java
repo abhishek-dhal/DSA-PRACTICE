a275a2867b2ab1a4b211c307ac5085b57c174c1f
abhishek-dhal <cse.22bcsh50@silicon.ac.in>
2026-02-25 11:13:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -1,17 +1,15 @@
 class Solution {
     // Function to calculate factorial of a number.
-    int factorial(int n) {
-       
-       
-       if(n==0) return 1;
-       
-       int fact=1;
-       
-       for(int i=2;i<=n;i++){
-           fact=fact*i;
-       }
-       
-       return fact;
+    int fc(int n){
+        if(n == 0 || n == 1) return 1;
         
+        
+        
+        return n * fc(n-1); 
+    }
+    int factorial(int n) {
+        // code here
+        
+       return fc(n);
     }
 }
